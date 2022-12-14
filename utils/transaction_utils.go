dc5481c1d2b52bc8d9d3e19a77b6e3481b4c9568
nshuman <0xnshuman@protonmail.com>
2022-12-13 22:03:03 -0700

    Add a helper transaction builder that uses eth client to set gas and nonce params

---
+++ utils/transaction_utils.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"math/big"
+
+	"github.com/0xNSHuman/dapp-tools/client"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+)
+
+func EncodeTransaction(
+	client *client.Client,
+	from common.Address,
+	to common.Address,
+	value *big.Int,
+	calldata []byte,
+) (*types.Transaction, error) {
+	callMessage, err := client.CreateCallMessage(from, to, value, calldata)
+	if err != nil {
+		return nil, err
+	}
+
+	tx, err := client.CreateTransaction(*callMessage)
+	if err != nil {
+		return nil, err
+	}
+
+	return tx, err
+}
