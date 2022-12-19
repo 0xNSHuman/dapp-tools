e5b8d1b34f0c4854b1788d4034f335abe838f88f
nshuman <0xnshuman@protonmail.com>
2022-12-18 20:45:19 -0700

    Add meta error domain to use across packages

--- common/types.go
+++ common/types.go
@@ -4,6 +4,23 @@ type ErrorDomain uint
 
 const (
 	ErrorDomainUndefined = iota * 100_000
+	ErrorDomainMeta
 	ErrorDomainClient
 	ErrorDomainWallet
 )
+
+type MetaError uint
+
+const (
+	Unknown MetaError = ErrorDomainMeta + iota
+	NotSupported
+)
+
+func (e MetaError) Error() string {
+	switch e {
+	case NotSupported:
+		return "Not supported"
+	default:
+		return "Unknown"
+	}
+}
